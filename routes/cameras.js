712d3a7587a9f3594afdd6a3839c16a17f5b3c3c
Axel von Bertoldi <bertoldia@gmail.com>
2015-01-03 13:10:15 -0700

    Terminate the image stream if the request is disconnected
    Basically we want to stop streaming the image feed if the client closes
    the browser window. Connect the request "end" and "close" signals to a
    method that (tries really hard to) terminate the stream.
    
    Although this seems to work, I'm not sure which of the statements in
    closeStream() does the trick, and note that it can still take some time
    (many seconds) for the stream to actually stop streaming data.

--- routes/cameras.js
+++ routes/cameras.js
@@ -9,9 +9,32 @@ function errorHandler(camCfg) {
   };
 }
 
+/*
+ * Forcibly terminate the stream. I'm not sure which combination of statements
+ * does the trick.
+ */
+function closeStream(stream) {
+  return function() {
+    if (stream) {
+      console.log("destroyed stream " + stream.feed.url);
+      stream.end();
+      stream.abort();
+      stream.destroy();
+      delete stream;
+      stream = null;
+    }
+  };
+}
+
 function streamCamera(camCfg, errorCB) {
   return function (req, res) {
-    request.get(camCfg.feed.url, camCfg).on('error', errorCB).pipe(res);
+    // make a camera stream request and pipe it to the response object.
+    var stream = request.get(camCfg.feed.url, camCfg);
+    stream.on('error', errorCB);
+    stream.pipe(res, {autoClose: true});
+    // terminate the stream if the request is disconnected.
+    req.on("close", closeStream(stream));
+    req.on("end", closeStream(stream));
   };
 }
 
