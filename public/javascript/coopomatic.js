fdc58c559ae3a97c4c5880cbf2e0e2eadfd87930
Axel von Bertoldi <bertoldia@gmail.com>
2015-02-09 20:28:05 -0700

    Periodic sensor reading
    Add the reading containers dynamically from the config, take an initial
    reading and setup polling.

--- public/javascript/coopomatic.js
+++ public/javascript/coopomatic.js
@@ -1,8 +1,34 @@
 $(window).ready(function() {
   $.getJSON("/config", function(config) {
     addCameraSelectors(config);
+    addSensorReadings(config);
   });
 });
+
+function addSensorReadings(config) {
+  $(config.sensors).each(function() {
+    $("<span/>", {
+      "id": this.id,
+      "class": "sensor-reading",
+      "text": this.description
+    }).appendTo("#sensors");
+    var cb = sensorReadingCallback(this);
+    readSensor(this, cb)();
+    setInterval(readSensor(this, cb), this.reading.period * 1000);
+  });
+}
+
+function readSensor(sensor, callback) {
+  return function() {
+    $.getJSON(sensor.reading.route, callback);
+  };
+}
+
+function sensorReadingCallback(sensor) {
+  return function(reading) {
+    $('#' + sensor.id).text(sensor.description + ": " + reading.result);
+  };
+}
 
 function addCameraSelectors(config) {
   $(config.cameras).each(function() {
